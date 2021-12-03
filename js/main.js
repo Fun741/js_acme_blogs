22a70d84bb2df7c8feb92b56f53306a51ef55966
Fun741 <89957391+Fun741@users.noreply.github.com>
2021-12-02 23:46:48 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,3 +28,35 @@ function createSelectOptions(user = "undefined") {
     return newArr;
 }
 
+function toggleCommentSection()
+{
+
+    return;
+}
+
+//a.Receives a postId as the parameter
+//b.Selects the section element with the data - post - id attribute equal to the postId received as a parameter
+//c.Use code to verify the section exists before attempting to access the classList property
+//d.At this point in your code, the section will not exist.You can create one to test if desired.
+//e.Toggles the class 'hide' on the section element
+//f.Return the section element
+
+function toggleCommentButton() {
+
+    return;
+}
+
+ 
+//a.Receives a parentElement as a parameter
+//b.Define a child variable as parentElement.lastElementChild
+//c.While the child exists…(use a while loop)
+//d.Use parentElement.removeChild to remove the child in the loop
+//e.Reassign child to parentElement.lastElementChild in the loop
+//f.Return the parentElement
+
+function deleteChildElements()
+{
+
+    return;
+}
+
