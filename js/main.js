1fdd8c130c9e91106f6afd196e3af42a9cc43aad
Fun741 <89957391+Fun741@users.noreply.github.com>
2021-12-07 21:48:35 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -102,16 +102,31 @@ function toggleCommentButton() {
 //e.Reassign child to parentElement.lastElementChild in the loop
 //f.Return the parentElement
 
-function deleteChildElements()
+function deleteChildElements(parentElement)
 {
-    let child = parentElement.lastElementChild;
-    while (child.firstChild)
+    // if element is undefined
+    if (parentElement == undefined || parentElement == ' ')
     {
-        child = parentElement.removeChild(child);
-        child = parentElement.lastElementChild(child);
+        // return normal html
+        return undefined;
     }
-
-    return parentElem;
+    else
+    {
+        // iterate until element has chile
+        while (parentElement.hasChildNodes())
+        {
+            // iterete until element child has child
+            while (parentElement.lastElementChild.hasChildNodes())
+            {
+                // remove child from first
+                //console.log(parentElement.lastElementChild.removeChild(parentElement.lastElementChild.firstChild));
+                parentElement.lastElementChild.removeChild(parentElement.lastElementChild.firstChild);
+            }
+            // remove element
+            html_element.removeChild(parentElement.lastElementChild);
+        }
+    }
+    return parentElement;
 }
 
 /*
