e54d893a353d020e0a080939827e7b0dbe699250
Fun741 <89957391+Fun741@users.noreply.github.com>
2021-12-08 19:32:05 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ function remove() {
 
 function deleteChildElements(parentElement)
 {
-    if (parentElement == undefined || html_element == ' ') { return undefined; }
+    if (parentElement == undefined || parentElement == ' ') { return undefined; }
 
     let child = parentElement.lastElementChild;
     while (child) {
